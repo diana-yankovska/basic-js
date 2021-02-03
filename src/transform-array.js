34c3220d7050bf17d3f68fe0ede05642fef7938e
Diana Yankovskaya <dianashpandaruk@mail.ru>
2021-02-03 16:47:52 +0300

    feat: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error("Input argument is not array");
+  }
+  let resultArr = [];
+
+  for (i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--double-prev": {
+        if (resultArr.length !== 0) {
+          if (i - 2 < 0 || arr[i - 2] !== "--discard-next") {
+            resultArr.push(resultArr[resultArr.length - 1]);
+          }
+        }
+        break;
+      }
+      case "--discard-prev": {
+        if (i - 2 < 0 || arr[i - 2] !== "--discard-next") {
+          resultArr.pop();
+        }
+        break;
+      }
+      case "--double-next": {
+        i++;
+        if (i < arr.length) {
+          resultArr.push(arr[i]);
+          resultArr.push(arr[i]);
+        }
+        break;
+      }
+      case "--discard-next": {
+        i++;
+        break;
+      }
+      default: {
+        resultArr.push(arr[i]);
+      }
+    }
+  };
+  return resultArr;
+}
+
