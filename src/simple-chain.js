b358ec99e0f67ae555180b3a52c43fe7aee3e9a1
Diana Yankovskaya <dianashpandaruk@mail.ru>
2021-02-10 10:04:15 +0300

    feat: implement chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value !== undefined) {
+      this.chain.push(String(value));
+    }
+
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (isNaN(position) || position !== Math.trunc(position) || position <= 0 || position > this.chain.length) {
+      this.chain.length = 0;
+      throw new Error("Invalid position");
+    }
+    
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = "( " + this.chain.join(" )~~( ") + " )";
+    this.chain.length = 0;
+    return result;
   }
 };
 
